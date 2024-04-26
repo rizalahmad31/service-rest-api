e0d6bf804e826aabdd2e79a9edcbc0a714d27d9a
rizalahmad31 <rizalahmad931@gmail.com>
2024-04-26 21:50:00 +0700

    percobaan kedua

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print ("hello world")
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Gaes"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/registrasi")
+def read_root():
+    return {"Hello": "Gaes"}
